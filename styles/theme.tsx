a74770ed869a92ab4b36af24b27f09cb5fd27428
SimHoZebs <simhozebs@gmail.com>
2021-04-20 02:37:52 +0900

    styling initialization

--- styles/theme.tsx
+++ styles/theme.tsx
@@ -1,9 +1,33 @@
-import { createMuiTheme } from '@material-ui/core/styles';
+import { createMuiTheme } from "@material-ui/core/styles";
 
 // Create a theme instance.
 const theme = createMuiTheme({
+  overrides: {
+    MuiCssBaseline: {
+      "@global": {
+        html: {
+          fontSize: "1px",
+        },
+        body: {
+          color: "hsl(255,11%,22%)",
+          fontSize: "18px",
+          fontFamily: "'Poppins', sans-serif",
+          fontWeight: 500,
+        },
+      },
+    },
+  },
   palette: {
-  }
+    primary: {
+      main: "hsl(180, 66%, 49%)",
+    },
+    secondary: {
+      main: "hsl(257, 27%, 26%)",
+    },
+    error: {
+      main: "hsl(0, 87%, 67%)",
+    },
+  },
 });
 
 export default theme;
