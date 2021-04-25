17f35f5a384cc2f320849cce871d3e0479b4dcfa
SimHoZebs <simhozebs@gmail.com>
2021-04-25 16:55:37 +0900

    frontend for input field without API - Increased input field border width

--- layout/Cta.tsx
+++ layout/Cta.tsx
@@ -1,20 +1,44 @@
+import React, { useState } from "react";
+
 import style from "../styles/css/cta.module.css";
 import { Button } from "@material-ui/core";
 
 const Cta: React.FC = () => {
+  const [link, setLink] = useState("");
+  const [isError, setIsError] = useState(false);
+
+  function shorten(
+    e:
+      | React.FormEvent<HTMLFormElement>
+      | React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) {
+    e.preventDefault();
+
+    if (link === "") {
+      setIsError(true);
+    } else {
+      //use API
+    }
+  }
+
   return (
-    <div className={style.cta}>
+    <form className={style.cta} onSubmit={(e) => shorten(e)}>
       <input
-        className={style.cta__input}
+        onChange={(e) => setLink(e.target.value)}
+        className={`${style.cta__input} ${isError && style.cta__input_error}`}
         type="text"
         placeholder="Shorten a link here..."
       />
-      <div className={style.cta__btnContainer}>
-        <Button id={style.cta__btn} variant="contained" color="primary">
-          Shorten it!
-        </Button>
-      </div>
-    </div>
+
+      <Button
+        id={style.cta__btn}
+        onClick={(e) => shorten(e)}
+        variant="contained"
+        color="primary"
+      >
+        Shorten it!
+      </Button>
+    </form>
   );
 };
 
