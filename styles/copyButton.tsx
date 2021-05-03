5c8762131ad11426f0a47ed0fabe247100ba294c
SimHoZebs <simhozebs@gmail.com>
2021-05-03 20:02:27 +0900

    Responsive shortened Link Container #85vw3x[Done]

--- styles/copyButton.tsx
+++ styles/copyButton.tsx
@@ -2,11 +2,14 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
   test: {
-    marginLeft: "1rem",
     borderRadius: "0.5rem",
     paddingTop: "0.5rem",
     paddingBottom: "0.5rem",
     width: "6.375rem",
+
+    "@media (max-width: 768px)": {
+      width: "100%",
+    },
   },
 
   pressed: {
