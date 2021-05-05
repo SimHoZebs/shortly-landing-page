468e2f58864d81fe0063eaa018b4c0e8b0ad1167
SimHoZebs <simhozebs@gmail.com>
2021-05-05 22:48:47 +0900

    Handling links that are too long to show

--- components/ShortenedLinks.tsx
+++ components/ShortenedLinks.tsx
@@ -1,4 +1,5 @@
-import { Button } from "@material-ui/core";
+import React, { useState, useEffect, useRef } from "react";
+
 import style from "../styles/css/shortenedLinks.module.css";
 import CopyButton from "./CopyButton";
 interface props {
@@ -9,24 +10,58 @@ interface props {
 
 function ShortenedLinks({ inputLinks, shortLinks, linkLoading }: props) {
   const shortLinksLastIndex = shortLinks.length - 1;
+  const [inputLinkMaxChar, setInputLinkMaxChar] = useState(100);
+  const [shortLinkMaxChar, setShortLinkMaxChar] = useState(100);
+
+  const inputLinkWrapperRef = useRef<HTMLDivElement | null>(null);
+  const shortLinkWrapperRef = useRef<HTMLDivElement | null>(null);
+
+  function handleResizeEvent() {
+    setInputLinkMaxChar(
+      Math.floor((inputLinkWrapperRef.current?.offsetWidth || 0) / 11)
+    );
+    setShortLinkMaxChar(
+      Math.floor((shortLinkWrapperRef.current?.offsetWidth || 0) / 11)
+    );
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", handleResizeEvent);
+
+    return () => window.removeEventListener("resize", handleResizeEvent);
+  }, []);
 
   return (
     <div className={style.container}>
       {linkLoading && (
         <img className={style.loadingImg} src="/images/loading.svg" alt="" />
       )}
-      {}
 
       {shortLinks.map((_, index) => {
         const reverseIndex = shortLinksLastIndex - index;
-        const shortLink = shortLinks[reverseIndex];
+        let shortLink = shortLinks[reverseIndex];
+        let inputLink = inputLinks[reverseIndex];
+
+        shortLink =
+          shortLink.length > shortLinkMaxChar
+            ? `${shortLink.slice(0, shortLinkMaxChar)} ...`
+            : shortLink;
+
+        inputLink =
+          inputLink.length > inputLinkMaxChar
+            ? `${inputLink.slice(0, inputLinkMaxChar)}...`
+            : inputLink;
 
         return (
           <div key={reverseIndex} className={style.block}>
-            <p className={style.block__reqLink}>{inputLinks[reverseIndex]}</p>
+            <div ref={inputLinkWrapperRef} className={style.linkWrapper}>
+              <p>{inputLink}</p>
+            </div>
 
             <div className={style.block__right}>
-              <p className={style.block__shortLink}>{shortLink}</p>
+              <div ref={shortLinkWrapperRef} className={style.shortLinkWrapper}>
+                <p>{shortLink}</p>
+              </div>
               <CopyButton shortLink={shortLink} />
             </div>
           </div>
